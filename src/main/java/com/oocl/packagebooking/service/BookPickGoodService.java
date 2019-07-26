27a597b644da99fb146a362db1c1345dcad6d514
scauzml <13828607279@163.com>
2019-07-26 08:10:04 +0800

    package put to change state   api complete

--- src/main/java/com/oocl/packagebooking/service/BookPickGoodService.java
+++ src/main/java/com/oocl/packagebooking/service/BookPickGoodService.java
@@ -1,9 +1,13 @@
 package com.oocl.packagebooking.service;
 
 import com.oocl.packagebooking.dao.BookPickGoodResponsity;
+import com.oocl.packagebooking.dao.PackAgeResponsity;
 import com.oocl.packagebooking.entity.BookPickGood;
+import com.oocl.packagebooking.entity.PackAge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BookPickGoodService {
@@ -11,9 +15,16 @@ public class BookPickGoodService {
     @Autowired
     BookPickGoodResponsity bookPickGoodResponsity;
 
-
+    @Autowired
+    PackAgeResponsity packAgeResponsity;
     public BookPickGood save(BookPickGood bookPickGood) {
         BookPickGood bookPickGood1 = bookPickGoodResponsity.save(bookPickGood);
+        Optional<PackAge> packAgeOptional = packAgeResponsity.findById(bookPickGood.getId());
+        if (packAgeOptional.isPresent()) {
+            PackAge packAge = packAgeOptional.get();
+            packAge.setState("已预约");
+            packAgeResponsity.save(packAge);
+        }
         return bookPickGood1;
 
     }
